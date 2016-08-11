9cf7eaf206715a233ce8f810c40a15e53f4de2cb
VanessaHahn <vanessa.kukko@googlemail.com>
2016-08-11 17:59:30 +0200

    GPS

--- app/src/main/java/mi/ur/de/android/runnersmeetup/RMU_Main.java
+++ app/src/main/java/mi/ur/de/android/runnersmeetup/RMU_Main.java
@@ -1,13 +1,81 @@
 package mi.ur.de.android.runnersmeetup;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RMU_Main extends AppCompatActivity {
+    private Button startButton;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rmu__main);
+        startButton = (Button) findViewById(R.id.startButton);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //weg+neue Strecke  location.longitude...
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{
+                        Manifest.permission.INTERNET, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                return;
+            }
+        } else {
+            configureButton();
+        }
+    }
+
+        @Override
+        public void onRequestPermissionsResult( int requestCode, String[] permissions, int[] grantResults){
+            switch (requestCode) {
+                case 1:
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        configureButton();
+                    }
+                    return;
+            }
+        }
+
+    private void configureButton() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationManager.requestLocationUpdates("gps", 5000, 2, locationListener);
+            }
+        });
     }
 }
+
