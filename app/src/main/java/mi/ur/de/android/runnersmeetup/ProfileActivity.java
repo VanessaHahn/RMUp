8f2d4b7f7485aadc5adad0f5795aeb165da54133
Theresa Strohmeier <theresa.strohmeier@web.de>
2017-09-18 23:42:48 +0200

    verbesserung

--- app/src/main/java/mi/ur/de/android/runnersmeetup/ProfileActivity.java
+++ app/src/main/java/mi/ur/de/android/runnersmeetup/ProfileActivity.java
@@ -30,8 +30,6 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         Intent i = getIntent();
         Bundle extras = i.getExtras();
