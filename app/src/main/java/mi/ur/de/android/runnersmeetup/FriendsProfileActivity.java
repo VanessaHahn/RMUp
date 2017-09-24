109f5a17ad1fe2fc92205ba7cdce3e10d4a2162f
VanessaHahn <vanessa.hahn@stud.uni-r.de>
2017-09-24 23:46:34 +0200

    friendsProfile

--- app/src/main/java/mi/ur/de/android/runnersmeetup/FriendsProfileActivity.java
+++ app/src/main/java/mi/ur/de/android/runnersmeetup/FriendsProfileActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -28,23 +27,45 @@ public class FriendsProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initLayout();
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},1);
+        initDeleteFriendship();
+        showProfile();
+        initSmsButton();
+        searchForEvents();
+    }
+
+    private void initLayout() {
         setContentView(R.layout.activity_friends_profile);
         listView = (ListView) findViewById(R.id.listFriendsEvents);
+        Intent i = getIntent();
+        Bundle extras = i.getExtras();
+        username = extras.getString(getString(R.string.username));
+        showName = (TextView) findViewById(R.id.showFriendUsername);
+        showDate = (TextView) findViewById(R.id.showFriendGeburtsdatum);
+        showGeschlecht = (TextView) findViewById(R.id.showFriendGeschlecht);
+        showEmail = (TextView) findViewById(R.id.showFriendEmail);
+        showGeschwindigkeit = (TextView) findViewById(R.id.showFriendGeschwindigkeit);
+        showVeranstaltungen = (TextView) findViewById(R.id.textView20);
+        showVeranstaltungen.setText(username+"s Veranstaltungen:");
+    }
+
+    private void initDeleteFriendship() {
         FloatingActionButton delete = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder deleteFriend = new AlertDialog.Builder(FriendsProfileActivity.this);
                 ActivityCompat.requestPermissions(FriendsProfileActivity.this,new String[]{Manifest.permission.SEND_SMS},1);
-                deleteFriend.setMessage("Freundschaft zu " + username +" löschen?")
+                deleteFriend.setMessage(R.string.friendshipWith + username +getString(R.string.delete))
                         .setCancelable(false)
-                        .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+                        .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                    deleteFriendship();
+                                deleteFriendship();
                             }
                         })
-                        .setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                        .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.cancel();
@@ -54,26 +75,13 @@ public class FriendsProfileActivity extends AppCompatActivity {
                 alert.show();
             }
         });
-        Intent i = getIntent();
-        Bundle extras = i.getExtras();
-        username = extras.getString("Username");
-        Log.d("Username",username);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},1);
+    }
 
-        showName = (TextView) findViewById(R.id.showFriendUsername);
-        showDate = (TextView) findViewById(R.id.showFriendGeburtsdatum);
-        showGeschlecht = (TextView) findViewById(R.id.showFriendGeschlecht);
-        showEmail = (TextView) findViewById(R.id.showFriendEmail);
-        showGeschwindigkeit = (TextView) findViewById(R.id.showFriendGeschwindigkeit);
-        showVeranstaltungen = (TextView) findViewById(R.id.textView20);
-
-        showVeranstaltungen.setText(username+"s Veranstaltungen:");
-
+    private void showProfile() {
         BackgroundWorker backgroundworker = new BackgroundWorker(this);
         AsyncTask<String, Void, String[]> returnAsyncTask = backgroundworker.execute("showProfil",username);
         try {
             String dbString = returnAsyncTask.get()[1];
-            Log.d("dbString",""+dbString);
             if(dbString.indexOf("/")>0){
                 String[] string = dbString.split("[/]");
                 showName.setText(username);
@@ -84,14 +92,16 @@ public class FriendsProfileActivity extends AppCompatActivity {
                 handynummer = string[4];
                 showGeschwindigkeit.setText(string[5] +" km/h");
             } else{
-                Log.d("RegisterActivity", "Registration failed!");
+                //failed
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+    }
 
+    private void initSmsButton() {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.friendfab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,9 +109,9 @@ public class FriendsProfileActivity extends AppCompatActivity {
 
                 AlertDialog.Builder logout = new AlertDialog.Builder(FriendsProfileActivity.this);
                 ActivityCompat.requestPermissions(FriendsProfileActivity.this,new String[]{Manifest.permission.SEND_SMS},1);
-                logout.setMessage("SMS senden an: " + username +"?")
+                logout.setMessage(R.string.sendSms + username +"?")
                         .setCancelable(false)
-                        .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 if(handynummer.isEmpty()) {
@@ -111,7 +121,7 @@ public class FriendsProfileActivity extends AppCompatActivity {
                                 }
                             }
                         })
-                        .setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.cancel();
@@ -122,9 +132,6 @@ public class FriendsProfileActivity extends AppCompatActivity {
                 alert.show();
             }
         });
-
-        searchForEvents();
-
     }
 
     private void searchForEvents() {
@@ -132,33 +139,26 @@ public class FriendsProfileActivity extends AppCompatActivity {
         AsyncTask<String, Void, String[]> returnAsyncTask = backgroundworker.execute("showEvents",id);
         try {
             String dbString = returnAsyncTask.get()[1];
-            Log.d("string1",""+dbString);
+            Log.d("event",""+dbString);
             if(dbString.indexOf("/")>0){
-                Log.d("hallo","hallo");
-                String[] dbString1 = dbString.split("[.]");
+                String[] dbStrings = dbString.split("[.]");
 
                 ArrayList<Event> eventlist = new ArrayList<>();
 
-                int eventeintraege = dbString1.length;
+                int eventeintraege = dbStrings.length;
                 Log.d("eventeintraege",""+eventeintraege);
                 if(eventeintraege>10){
                     eventeintraege = 10;
                 }
 
                 for (int i = 0; i<eventeintraege; i++) {
-                    String[] string1 = new String[dbString1[i].length()];
+                    String[] eventItem = new String[dbStrings[i].length()];
 
                     for (int x = 0; x < 4; x++) {
-                        string1 = dbString1[i].split("[/]");
+                        eventItem = dbStrings[i].split("[/]");
                     }
 
-                    Log.d("string1",""+string1[0]);
-                    Log.d("string1",""+string1[1]);
-                    Log.d("string1",""+string1[2]);
-                    Log.d("string1",""+string1[3]);
-                    Log.d("string1",""+string1[4]);
-
-                    Event event = new Event(string1[0],string1[1],string1[2],string1[3],string1[4]);
+                    Event event = new Event(eventItem[0],eventItem[1],eventItem[2],eventItem[3],eventItem[4]);
                     eventlist.add(event);
                 }
 
@@ -166,16 +166,14 @@ public class FriendsProfileActivity extends AppCompatActivity {
                 listView.setAdapter(adapter);
 
             } else{
-                Toast.makeText(this, "Keine Veranstaltungen erstellt!", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.noEventCreated, Toast.LENGTH_LONG).show();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
             // Not successful
-            Log.d("RegisterActivity", "Registration  failed!");
         } catch (ExecutionException e) {
             e.printStackTrace();
             // Not successful
-            Log.d("RegisterActivity", "Registration  failed!");
         }
     }
 
@@ -185,31 +183,29 @@ public class FriendsProfileActivity extends AppCompatActivity {
         AsyncTask<String, Void, String[]> returnAsyncTask = backgroundworker.execute("deleteFriendship",userID, id);
         try {
             String dbString = returnAsyncTask.get()[1];
-            if(dbString.equals("true")){
-                Toast.makeText(this, "Freundschaft zu " + username + " gelöscht!", Toast.LENGTH_LONG).show();
+            if(dbString.equals("true")) {
+                Toast.makeText(this, getString(R.string.friendshipWith) + username + getString(R.string.deleted), Toast.LENGTH_LONG).show();
                 startActivity(new Intent(this, FriendsActivity.class));
             } else{
-                Toast.makeText(this, "Freundschaft konnte nicht gelöscht werden!", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, R.string.couldNotDeleteFriendship, Toast.LENGTH_LONG).show();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
             // Not successful
-            Log.d("RegisterActivity", "Registration  failed!");
         } catch (ExecutionException e) {
             e.printStackTrace();
             // Not successful
-            Log.d("RegisterActivity", "Registration  failed!");
         }
     }
 
     private void smsSenden(){
         SmsManager manager = SmsManager.getDefault();
-        String sms = "Hallo, ich bin " + Constants.getName() + " von RunnersMeetUp und möchte mit dir Kontakt aufnehmen!";
-        Toast.makeText(this, "SMS gesendet!", Toast.LENGTH_LONG).show();
+        String sms = R.string.salutation + Constants.getName() + R.string.contacting;
+        Toast.makeText(this, R.string.sendedSms, Toast.LENGTH_LONG).show();
         manager.sendTextMessage(handynummer,null,sms,null,null);
     }
 
     private void toast(){
-        Toast.makeText(this, "SMS senden nicht möglich!", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.smsNotPossible, Toast.LENGTH_LONG).show();
     }
 }
